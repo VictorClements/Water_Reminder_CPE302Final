5566388a420bb1134a23ddd31c0c472a08fbe58d
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-10-22 00:05:49 -0700

    Update bcdCounter.sv

--- bcdCounter.sv
+++ bcdCounter.sv
@@ -1,30 +1,3 @@
-/*      //more explicit with what happens at each clock edge
-module bcdCounter 
-  #(parameter Max = 4'd9)
-   (input  logic       clk, reset,
-    output logic [3:0] count,
-    output logic       carryOut);
-
-    always_ff @(posedge clk)
-        if (reset)              begin
-                                        count <= 4'd0;
-                                        carryOut <= 1'b0;
-        end
-        else if(count == Max)   begin
-                                        count <= 4'd0;
-                                        carryOut <= 1'b0;
-        end
-        else if(count == Max-1) begin
-                                        count <= count + 1;
-                                        carryOut <= 1'b1;
-        end
-        else                    begin             
-                                        count <= count + 1;
-                                        carryOut <= 1'b0;
-        end
-endmodule
-*/
-/*
 //this doesnt account for the the fact that the signals are output at the start of the change to the maximum, not the end
 module bcdCounter 
   #(parameter Max = 4'd9)
@@ -42,7 +15,7 @@ module bcdCounter
         else if(count == Max-1)         {count, carryOut} <= {count+1, 1'b1};
         else                            {count, carryOut} <= {count+1, 1'b0};
 endmodule
-*/
+
 //try out the method of the enable for the carryOut
 
 module secondsCounter (input  logic       clk, reset,
