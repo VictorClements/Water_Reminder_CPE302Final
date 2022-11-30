ee7fa40b7daa30cb4d3602d3a4eb3a541a9e5b5f
VictorClements <104516656+VictorClements@users.noreply.github.com>
2022-11-30 01:15:06 -0800

    Update vgawrapper.sv

--- vgawrapper.sv
+++ vgawrapper.sv
@@ -1,8 +1,6 @@
-// DE2-115 wrapper
-
-
 module vgawrapper(input  logic       CLOCK_50,
-                  input  logic [6:0] SW,
+                  input  logic [2:0] SW,
+						      inout  logic [4:0] GPIO,
                   output logic       VGA_CLK, 
                   output logic       VGA_HS,
                   output logic       VGA_VS,
@@ -11,11 +9,10 @@ module vgawrapper(input  logic       CLOCK_50,
                   output logic [7:0] VGA_R,
                   output logic [7:0] VGA_G,
                   output logic [7:0] VGA_B,
-                  output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+                  output logic [6:0] HEX0, HEX1, HEX2, HEX3);
 						
-
-  vga vga(CLOCK_50, SW[0],SW[4:1],SW[6:5], VGA_CLK, VGA_HS, VGA_VS, VGA_SYNC_N, VGA_BLANK_N,
-			 VGA_R, VGA_G, VGA_B, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+  vga vga(CLOCK_50, SW[0], {GPIO[0], GPIO[1], GPIO[2], GPIO[3]},
+          SW[2:1], VGA_CLK, VGA_HS, VGA_VS, VGA_SYNC_N, VGA_BLANK_N,
+		      GPIO[4], VGA_R, VGA_G, VGA_B, HEX0, HEX1, HEX2, HEX3);
 			 
 endmodule
-
